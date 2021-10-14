8c28c8eac4476fb98978b0905c877442e86d39bf
Mateo Rodriguez <mateo.rodriguez.fierro@gmail.com>
2021-10-14 17:01:23 -0500

    header terminado

---
+++ dalet/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import './sass/App.scss';
+
+function App() {
+  return (
+   <>
+    <Header/>
+   </>
+  );
+}
+
+export default App;
---
+++ dalet/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Header =()=>{
+    return(
+        <>
+            <header>
+              <nav className="grid">
+                <ul>
+                    <li><a href="#mision-vision">Quien soy</a></li>
+                    <li><a href="#quien-soy">Mis servicios</a></li>
+                    <li><a href="#cursos">Dalet</a></li>
+                    <li><a href="#proximamente">experiencia</a></li>
+                    <li><a href="#donde-estoy">contactame</a></li>
+                </ul>
+              </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
