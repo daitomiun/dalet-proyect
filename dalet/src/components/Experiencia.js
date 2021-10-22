48ff6039864f648e3f0724ef5b5bd645f4680243
Mateo Rodriguez <mateo.rodriguez.fierro@gmail.com>
2021-10-22 15:04:11 -0500

    slider a mitad de camino

--- dalet/src/components/Experiencia.js
+++ dalet/src/components/Experiencia.js
@@ -1,11 +1,41 @@
 import React from "react";
-
+import button from "../assets/right-arrow.png";
 
 const Experiencia = () => {
   return (
     <>
-      <div className="background red"></div>
-      
+      <div className="background red">
+        <h1>experiencia</h1>
+        <div className="carousel">
+          <button className="carousel__button carousel__button--left">
+            <img className="left" srcset={button} alt=""></img>
+          </button>
+
+          <div className="carousel__track-container">
+            <ul className="carousel__track">
+              <li className="carousel__slide">
+                <img className="carousel__image" src="" alt="" srcset="" />
+              </li>
+              <li className="carousel__slide">
+                <img className="carousel__image" src="" alt="" srcset="" />
+              </li>
+              <li className="carousel__slide">
+                <img className="carousel__image" src="" alt="" srcset="" />
+              </li>
+            </ul>
+          </div>
+
+          <button className="carousel__button carousel__button--right">
+            <img className="right" src={button} alt=""></img>
+          </button>
+
+          <div className="carousel__nav">
+            <button className="carousel__indicator current-slide"></button>
+            <button className="carousel__indicator"></button>
+            <button className="carousel__indicator"></button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
